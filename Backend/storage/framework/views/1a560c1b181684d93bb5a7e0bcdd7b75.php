8c7d4054c008889ec2526547d270d47fd9bb4912
Joao-Pedro-Onofre <joao.p.s.onofre@gmail.com>
2025-09-25 11:39:10 +0100

    reset

--- Backend/storage/framework/views/1a560c1b181684d93bb5a7e0bcdd7b75.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH E:\Trabalho\MySite\Backend\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/table.blade.php ENDPATH**/ ?>
--- Backend/storage/framework/views/99fe10d540a73ab9ef6fa75485ecc683.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>: <?php echo e($url); ?>
-
-<?php /**PATH E:\Trabalho\MySite\Backend\resources\views/vendor/mail/text/header.blade.php ENDPATH**/ ?>
