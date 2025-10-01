8e42370efea6daa96995da6f7afd36a687e1baad
Joao-Pedro-Onofre <joao.p.s.onofre@gmail.com>
2025-10-01 18:23:23 +0100

    css fix

--- Frontend/src/sections/HomeSection.vue
+++ Frontend/src/sections/HomeSection.vue
@@ -5,8 +5,8 @@
 				<div class="text-center">
 					<h1 class="mx-auto my-0 text-uppercase">João Pedro Onofre</h1>
 					<h2 class="text-white-50 mx-auto mt-2 mb-5">Full-stack developer crafting web, backend, and business solutions with clean code and creative ideas</h2>
-					<a class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Contact Me!</a>
-					<a class="btn btn-primary" href="#CV">Get my CV!</a>
+					<a class="btn btn-primary m-3" data-bs-toggle="modal" data-bs-target="#exampleModal">Contact Me!</a>
+					<a class="btn btn-primary m-3" href="#CV">Get my CV!</a>
 				</div>
 			</div>
 		</div>
