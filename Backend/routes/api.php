863b10f57f0b9f3fc5bed738ab958131a0828ad1
Joao-Pedro-Onofre <joao.p.s.onofre@gmail.com>
2025-10-02 15:59:37 +0100

    request fix

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmailController;
 
 Route::post('email', [EmailController::class, 'insert']);
+Route::get('/health', fn() => response()->json(['ok' => true]));
